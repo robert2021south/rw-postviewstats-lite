05f7bec64c59b9010b227cd1d78e94400004dfd9
lgheric <saverneo@163.com>
2025-09-05 18:37:09 +0800

    修改PostViewsColumn类的is_pro_plugin_active()方法为protected

--- includes/modules/postcolumn/PostViewsColumn.php
+++ includes/modules/postcolumn/PostViewsColumn.php
@@ -33,7 +33,7 @@ class PostViewsColumn {
     }
 
     // 检测 Pro 插件是否启用
-    private function is_pro_plugin_active() {
+    protected function is_pro_plugin_active() {
         if (!function_exists('is_plugin_active')) {
             include_once ABSPATH . 'wp-admin/includes/plugin.php';
         }
