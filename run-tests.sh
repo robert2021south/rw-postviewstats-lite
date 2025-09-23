a7ee3464fd0a1e4524596d56f59a66d8a199d1c4
lgheric <saverneo@163.com>
2025-09-24 07:02:15 +0800

    调整yml和run-tests.sh以尝试解决ping不通mysql8的问题

--- run-tests.sh
+++ run-tests.sh
@@ -19,7 +19,11 @@ echo "Selenium: $WP_WEB_DRIVER_URL"
 echo ""
 
 echo "====== Waiting for MySQL ======"
-until mysqladmin ping -h "$WP_DB_HOST" --silent; do
+until mysqladmin ping -h "$WP_DB_HOST" -u"$WP_DB_USER" -p"$WP_DB_PASSWORD" --silent; do
+      echo "Waiting for MySQL..."
+      sleep 2
+  done
+
     echo "Waiting for MySQL..."
     sleep 2
 done
